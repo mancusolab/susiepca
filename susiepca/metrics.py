441e3b22ea3e722a5faafbccd688c3c3b47e2a88
Dong555 <dy71438@gmail.com>
2022-10-21 14:18:11 -0700

    update metric docu

--- susiepca/metrics.py
+++ susiepca/metrics.py
@@ -5,21 +5,29 @@ def mse(X: jnp.ndarray, Xhat: jnp.ndarray):
     """
 
 
-    Parameters
-    ----------
-    X : Input data. Should be a array-like
-    Xhat : Predicted data. Should be in the same shape with X
+    Args:
+        X : Input data. Should be a array-like
+        Xhat : Predicted data. Should be in the same shape with X
 
-    Returns
-    -------
-    rrmse: relative root mean square error
+    Returns:
+        RRMSE: relative root mean square error
 
     """
     return jnp.sum((X - Xhat) ** 2) / jnp.sum(X ** 2)
 
 
 def get_credset(params, rho=0.9):
-    """ """
+
+    """
+    Args:
+        params: the dictionary return from the function susie_pca
+        rho: the level from credible set, should ranged in (0,1)
+
+    Returns:
+        cs: credible set, which is a dictionary contain K*P credible sets
+
+    """
+
     l_dim, z_dim, p_dim = params.alpha.shape
     idxs = jnp.argsort(-params.alpha, axis=-1)
     cs = {}
