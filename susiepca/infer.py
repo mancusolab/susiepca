38ba716e7867d11a39d552598948e202654c7966
Dong555 <dy71438@gmail.com>
2022-10-23 17:40:41 -0700

    Update infer.py

--- susiepca/infer.py
+++ susiepca/infer.py
@@ -302,8 +302,7 @@ def compute_elbo(X, params) -> ELBOResults:
 
 def compute_pip(params):
 
-    """Create a function to compute the posterior
-        inclusion probabilities (PIPs).
+    """
 
     Args:
         params: the dictionary return from the function ``susie_pca``.
