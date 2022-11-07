c389259b26112d8d698b11de6213faeccffb6d9a
Dong555 <dy71438@gmail.com>
2022-11-06 18:24:42 -0800

    documentation and test for simulation function

--- susiepca/sim.py
+++ susiepca/sim.py
@@ -32,7 +32,11 @@ def generate_sim(
     z_dim: int,
     effect_size: float = 1.0,
 ) -> SimulatedData:
-    """Create the function to generate a sparse data for PCA.
+    """Create the function to generate a sparse data for PCA. Please note that to
+        illustrate how SuSiE PCA work, we build this simple and
+        straitforward simulation where each latent component have exact l_dim number
+        of non-overlapped single effects. Please make sure l_dim < p_dim/z_dim
+        when generate simulation data using this function.
 
     Args:
         seed: Seed for "random" initialization
@@ -49,6 +53,36 @@ def generate_sim(
     """
     rng_key = random.PRNGKey(seed)
     rng_key, z_key, b_key, obs_key = random.split(rng_key, 4)
+
+    # dimension check
+    if l_dim > p_dim:
+        raise ValueError(
+            f"l_dim should be less than p: received l_dim = {l_dim}, p = {p_dim}"
+        )
+    if l_dim > p_dim / z_dim:
+        raise ValueError(
+            f"""l_dim is smaller than p_dim/z_dim,
+            please make sure each component has {l_dim} single effects"""
+        )
+
+    if l_dim <= 0:
+        raise ValueError(f"l_dim should be positive: received l_dim = {l_dim}")
+
+    if z_dim > p_dim:
+        raise ValueError(
+            f"z_dim should be less than p: received z_dim = {z_dim}, p = {p_dim}"
+        )
+    if z_dim > n_dim:
+        raise ValueError(
+            f"z_dim should be less than n: received z_dim = {z_dim}, n = {n_dim}"
+        )
+    if z_dim <= 0:
+        raise ValueError(f"z_dim should be positive: received z_dim = {z_dim}")
+
+    if effect_size <= 0:
+        raise ValueError(
+            f"effect size should be positive: received effect_size = {effect_size}"
+        )
 
     Z = random.normal(z_key, shape=(n_dim, z_dim))
     W = jnp.zeros(shape=(z_dim, p_dim))
